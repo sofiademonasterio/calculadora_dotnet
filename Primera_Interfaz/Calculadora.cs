88edbaff2ece449b4275f84a922bd7cc00fa4caf
Sofía de Monasterio <14180854-sofiademonasterio1@users.noreply.gitlab.com>
2023-05-16 12:24:11 -0300

    Agregar archivos de proyecto.

---
+++ Primera_Interfaz/Calculadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Primera_Interfaz;
+
+namespace NCalculadora
+{
+    internal class Calculadora
+    {
+        //Constructor
+        public Calculadora()
+        {
+            primerOperador = "";
+            segundoOperador = "";
+            resultado = "";
+            operando = null;
+        }
+
+        //Propiedades
+        private string primerOperador;
+        private string segundoOperador;
+        private OperandoDelegate? operando;
+        private string resultado;
+
+        //Getter-setter
+        public string PrimerOperador { get => primerOperador; set => primerOperador = value; }
+        public string SegundoOperador { get => segundoOperador; set => segundoOperador = value; }
+        internal OperandoDelegate? Operando { get => operando; set => operando = value; }
+        public string Resultado { get => resultado; set => resultado = value; }
+
+        //Metodos estaticos para pasarse por valor afuera
+        public static double Suma(double x, double y) { return x + y; }
+        public static double Resta(double x, double y) { return x - y; }
+        public static double Multiplicacion(double x, double y) { return x * y; }
+        public static double Division(double x, double y) {
+            if (y == 0) return 0;
+            return x / y; 
+        }
+
+        //Metodos de la clase no estaticos
+        public void CalcularOperacion()
+        {
+            if (operando != null && segundoOperador != "")
+            {
+                resultado = $"{operando(double.Parse(primerOperador), double.Parse(SegundoOperador))}";
+            }
+        }
+    }
+}
---
+++ Primera_Interfaz/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NCalculadora;
+
+namespace Primera_Interfaz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextBlock? resultado;
+        TextBlock? contenido;
+        Calculadora calculadora = new Calculadora();
+        public MainWindow()
+        {
+            InitializeComponent();
+            resultado = FindName(name: "Resultado") as TextBlock;
+            contenido = FindName(name: "Contenido") as TextBlock;
+
+        }
+
+        //Boton de los numeros
+        private void Button_Number(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (calculadora.Operando == null)
+            {
+                calculadora.PrimerOperador = calculadora.PrimerOperador + btn.Content;
+                contenido.Text = calculadora.PrimerOperador;
+            }
+            else
+            {
+                calculadora.SegundoOperador += btn.Content;
+
+                contenido.Text += calculadora.SegundoOperador;
+            }
+                
+        }
+        
+        //Boton de los operando
+         private void Button_Operando(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (calculadora.Operando != null)
+            {
+                calculadora.SegundoOperador = "";
+                calculadora.PrimerOperador = calculadora.Resultado;
+            }
+                switch ($"{btn.Content}")
+                {
+                    case "+":
+                        calculadora.Operando = Calculadora.Suma;
+                        contenido.Text += "+";
+                        break;
+                    case "-":
+                        calculadora.Operando = Calculadora.Resta;
+                        contenido.Text += "-";
+                        break;
+                    case "*":
+                        calculadora.Operando = Calculadora.Multiplicacion;
+                        contenido.Text += "*";
+                        break;
+                    case "/":
+                        calculadora.Operando = Calculadora.Division;
+                        contenido.Text += "/";
+                        break;
+                }
+            }
+
+        //Boton del grid
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            calculadora.CalcularOperacion();
+            resultado.Text = calculadora.Resultado;
+        }
+    }
+    delegate double OperandoDelegate(double x, double y);
+}
